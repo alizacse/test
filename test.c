eef59efe79d1f440b4d3a56dc46a1bd135562fb0
alizacse <alizacse32@gmail.com>
2019-10-17 19:00:01 +0600

    add two number

---
+++ test.c
@@ -0,0 +1,14 @@
+public class Calculator{
+	public static void main(String[] args){
+
+		System.out.println(Hello);
+		
+	}
+       int  addition(int x, int double){
+		return x + y;
+	}
+	}
+
+
+
+	}
