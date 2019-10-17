c68adad3734def2c6248c204b7affec0366daa55
alizacse <alizacse32@gmail.com>
2019-10-17 19:11:16 +0600

    multiply two number

--- test.c
+++ test.c
@@ -1,14 +1,25 @@
 public class Calculator{
 	public static void main(String[] args){
 
-		System.out.println(Hello);
+		System.out.println(addition(3,2));
+
+		System.out.println(subtraction(3,2));
 		
 	}
-       int  addition(int x, int double){
+
+       int  addition(int x, int y)
+        {
 		return x + y;
 	}
+
+        int  multiplication(int x, int y)
+        {
+		return x *y;
+	}
+
+
 	}
 
 
 
-	}
+	
